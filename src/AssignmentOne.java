fd4f95d1b717a2f3d610de7d02b906665558d363
HanCX1 <2513760564@qq.com>
2025-11-10 14:54:28 +0800

    初始提交：创建AssignmentOne和HealthProfessional类

---
+++ src/AssignmentOne.java
@@ -0,0 +1,5 @@
+public class AssignmentOne{
+    public static void main(String[] args){
+        System.out.println("项目结构正确，可以开始后续开发了。");
+    }
+}   
---
+++ src/HealthProfessional.java
@@ -0,0 +1,14 @@
+public class HealthProfessional {
+    //必需的实例变量
+    private int id;
+    private String name;
+    private String department;
+
+    //默认构造函数
+    public HealthProfessional(){
+        this.id = 0;
+        this.name = "none";
+        this.department = "z"
+    }
+    
+}
